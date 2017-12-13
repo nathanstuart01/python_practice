625f04052e0747976709534b7dcfd82a6dff35ce
Nathan Stuart <nathan.stuart01@gmail.com>
2017-12-13 08:33:44 -0700

    practiced additional data visualation from csvs using matplot lib, started to work on data visualization from json files using pygal

---
+++ countries.py
@@ -0,0 +1,6 @@
+from pygal.maps.world import COUNTRIES
+from pygal_maps_world import i18n
+
+
+for country_code in sorted(COUNTRIES.keys()):
+	print(country_code, COUNTRIES[country_code])
--- highs_lows.py
+++ highs_lows.py
@@ -1,28 +1,37 @@
 import csv
-
+from datetime import datetime
 from matplotlib import pyplot as plt 
 
-# get high temperatures from file
+# get dates and high temperatures from file
 
-filename = 'sitka_weather_07-2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
 
-	highs = []
+	dates, highs, lows = [], [], []
 	for row in reader:
-		high = int(row[1])
-		highs.append(high)
-	print(highs)
-
+		try:
+			current_date = datetime.strptime(row[0], "%Y-%m-%d")
+			high = int(row[1])
+			low = int(row[3])
+		except ValueError:
+			print(current_date, 'Missing Data')
+		else:
+			highs.append(high)
+			lows.append(low)
+			dates.append(current_date)
 # Plot data
 
 fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
